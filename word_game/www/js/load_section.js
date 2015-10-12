b1c2e965e3d7a6f471b5cb6544ca13719946f506
evrimgurkan <e.evrimgurkan@gmail.com>
2015-10-12 16:18:33 +0300

    Fill UI with Json #3
    * Deleted hardcoded strings from index.html
    * Added section loader js and fill the UI with json data

---
+++ word_game/www/js/load_section.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    var descriptionArea = $('#descriptionArea');
+    var leftButton      = $('#leftButton');
+    var rightButton     = $('#rightButton');
+    var questionArea    = $('#questionArea');
+    var timerContainer  = $('#timerContainer');
+    var stepContainer   = $('#stepContainer');
+
+    var currentQuestionNumber = 1;
+
+    var s_id = 0;
+
+    $.ajax({
+        url: 'http://localhost/word_game_web_service.php?s_id=' + s_id,
+        dataType: 'jsonp',
+        jsonp: 'jsoncallback',
+        timeout: 5000,
+        success: function(data, status){
+
+            descriptionArea.append(data['S_DESC']);
+            leftButton.append(data['ANSWERS'][0]);
+            rightButton.append(data['ANSWERS'][1]);
+            questionArea.append(data['QUESTIONS']['Q_TEXT0']);
+            timerContainer.append(data['S_TIME']);
+
+            var step = currentQuestionNumber + "/" + data['Q_COUNT'];
+
+            stepContainer.append(step);
+       
+        },
+        error: function(){
+           output.text('There was an error loading the data.');
+        }
+    });
+
+});
