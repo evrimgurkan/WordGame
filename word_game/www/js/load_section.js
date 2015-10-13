209ae686bee40c9569e56c56c3eeccb352ace1cd
cursedbustah <ahmetceylandeu@gmail.com>
2015-10-13 08:37:11 +0300

    updated ui. added new fonts and rounded circles

--- word_game/www/js/load_section.js
+++ word_game/www/js/load_section.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+    initTimers(90);
+    updateStep(1,40);
     var descriptionArea = $('#descriptionArea');
     var leftButton      = $('#leftButton');
     var rightButton     = $('#rightButton');
@@ -8,7 +11,7 @@ $(document).ready(function(){
 
     var currentQuestionNumber = 1;
 
-    var s_id = 0;
+    var s_id = 1;
 
     $.ajax({
         url: 'http://localhost/word_game_web_service.php?s_id=' + s_id,
@@ -17,18 +20,18 @@ $(document).ready(function(){
         timeout: 5000,
         success: function(data, status){
 
-            descriptionArea.append(data['S_DESC']);
-            leftButton.append(data['ANSWERS'][0]);
-            rightButton.append(data['ANSWERS'][1]);
+            descriptionArea.append('Aşağıdaki Soruları Cevaplayın Layn!');
+            leftButton.append('Sayısal');
+            rightButton.append('Sözel');
 
             var questionNumberId = 'Q_TEXT' + (currentQuestionNumber - 1);
-            questionArea.append(data['QUESTIONS'][questionNumberId]);
-            timerContainer.append(data['S_TIME']);
+            questionArea.append('Ziraat Mühendisliği');
+            //timerContainer.append(90);
 
             var step = currentQuestionNumber + "/" + data['Q_COUNT'];
 
-            stepContainer.append(step);
-       
+            //stepContainer.append('3/40');
+
         },
         error: function(){
            output.text('There was an error loading the data.');
@@ -36,3 +39,37 @@ $(document).ready(function(){
     });
 
 });
+
+function initTimers(iTme)
+{
+    var time = iTme;
+    var initialOffset = '440';
+    var i = 1;
+    var timer_interval = setInterval(function() {
+        $('#timer_circle').css('stroke-dashoffset', initialOffset-(i*(initialOffset/time)));
+        document.getElementById('timerValue').innerHTML = i;
+
+        if (i == time) {
+            clearInterval(timer_interval);
+        }
+        i++;
+    }, 1000);
+}
+
+function updateStep(currentStep,totalStep)
+{
+    var step_count = totalStep;
+    var initialOffset = '440';
+    var step = currentStep;
+    //var step_interval = setInterval(function() {
+        $('#step_circle').css('stroke-dashoffset', initialOffset-(step*(initialOffset/step_count)));
+        document.getElementById('stepValue').innerHTML = step + '/' + step_count;
+
+        if (step == step_count) {
+            //clearInterval(step_interval);
+            alert('Reached Total Step');
+        }
+        step++;
+    //}, 1000);
+
+}
